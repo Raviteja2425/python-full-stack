4a68eb38cd514e53c64a9e4cf8cf1fa472f8359c
Raviteja2425 <doddiraviteja79@gmail.com>
2026-09-10 17:28:23 +0530

    Update practice session

--- practice session.py
+++ practice session.py
@@ -28,7 +28,6 @@ price=2500
 price+=500
 print (price)
 
-"""
 
 # Comparision --> COmpare the value <,>,<=,>=,==,!=
 # membership-->in,not in -->checks for the values in a collection
@@ -50,3 +49,70 @@ for price in Prices:
     elif price < 5000:
         final_price.append(price)
 print(final_price)
+
+
+
+# A teacher wants to build and update a simple list of student marks.
+
+### Important in programming : 1).Input 2).Output 3).Logic
+
+marks=[]
+for mark in range(3):
+    mark=int(input("Enter the marks : "))
+    marks.append(mark)
+#print(marks)
+marks.insert(0,90)
+marks.extend([75,85])
+if 75 in marks:
+    marks.remove(75)
+print(marks.pop())
+print("Final list is ",marks)
+print("Length of the list is ", len(marks))
+
+
+
+
+numbers=[20,50,75,98,65,45]
+print("Sorted list is: ")
+numbers.sort()
+print("Desc order of list is ")
+numbers.reverse()
+print(numbers)
+num=int(input("Enter your number: "))
+if num in numbers:
+    print("count is",numbers.count(num))
+    print("First Index is ",numbers.index(num))
+else:
+    print("number not found")
+"""
+
+numbers=[10,15,20,25,30,35]
+even=[]
+odd=[]
+
+for num in numbers:
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
+print(even)
+print(odd)
+
+print("First 3 elements",numbers[:3])
+print("Last 3 elements",numbers[-3:])
+f = numbers.copy()
+numbers.clear()
+print("original list",numbers)
+print("backup",f)
+
+
+
+
+
+
+
+
+
+
+
+    
